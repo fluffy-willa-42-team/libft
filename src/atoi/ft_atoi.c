e15b6cc219a7a2a9759dc69eb228e8f7c3cb3b3f
matthew-dreemurr <hadadmat@gmail.com>
2022-07-04 14:53:35 +0200

    [FIX] ft_atoi

--- src/atoi/ft_atoi.c
+++ src/atoi/ft_atoi.c
@@ -5,44 +5,64 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/01 16:46:59 by awillems          #+#    #+#             */
-/*   Updated: 2022/06/21 14:24:28 by mahadad          ###   ########.fr       */
+/*   Created: 2021/10/06 11:08:41 by mahadad           #+#    #+#             */
+/*   Updated: 2022/07/04 14:33:14 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <limits.h>
 #include "lib_is_check.h"
 
-/**
- * @brief Covert an string to an integer. Format : ""
- * 
- * @param str String of a format :
- * (Any whitespace) + (sign) + (Numbers) => " -42"
- * @return An integer from the original string.
- */
+/*
+* SYNOPSIS
+*   #include <stdlib.h>
+* 
+*   int
+*   atoi(const char *str);
+* 
+* DESCRIPTION
+*   The atoi() function converts the initial portion of the string pointed to
+*   by str to int representation.
+* 
+*   It is equivalent to:
+* 
+*     (int)strtol(str, (char **)NULL, 10);
+* 
+*   While the atoi() function uses the current locale, the atoi_l() function
+*   may be passed a locale directly. See xlocale(3) for more information.
+* 
+* IMPLEMENTATION NOTES
+*   The atoi() and atoi_l() functions are thread-safe and async-cancel-safe.
+* 
+*   The strtol() and strtol_l() functions are recommended instead of atoi()
+*   and atoi_l() functions, especially in new code.
+* 
+* ERRORS
+*   The function atoi() need not affect the value of errno on an error.
+*/
+
 int	ft_atoi(const char *str)
 {
-	int					i;
-	unsigned long int	res;
-	int					s;
+	unsigned long	nbr;
+	unsigned long	cutoff;
+	int				cutlim;
+	int				isneg;
 
-	i = 0;
-	s = 1;
-	while (ft_is_whitespace(str[i]))
-		i++;
-	if (str[i] == '+' || str[i] == '-')
+	nbr = 0;
+	isneg = 0;
+	while ((*str >= '\t' && *str <= '\r') || *str == ' ')
+		str++;
+	isneg = (*str == '-');
+	str += (isneg || *str == '+');
+	cutoff = (unsigned long)(LONG_MAX / 10);
+	cutlim = (int)(LONG_MAX % 10);
+	while (ft_isdigit((int)*str))
 	{
-		if (str[i] == '-')
-			s = -1;
-		i++;
+		if (nbr > cutoff || (nbr == cutoff && (int)(*str - '0') > cutlim))
+			return (-!isneg);
+		nbr *= 10;
+		nbr += (*str - '0');
+		str++;
 	}
-	res = 0;
-	while (ft_isdigit(str[i]))
-	{
-		if (res > LONG_MAX / 10 && str[i] - 48 > (LONG_MAX % 10) - (s - 1) / 2)
-			return (- (s + 1) / 2);
-		res = res * 10 + (str[i] - 48);
-		i++;
-	}
-	return ((int)((int)res * s));
+	return ((int []){nbr, -nbr}[isneg]);
 }
